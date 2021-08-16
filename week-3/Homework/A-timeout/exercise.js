189d64a9d137ab9311bf68848458a99c0ae4f609
wandraded <wandraded4@gmail.com>
2021-08-16 20:51:46 +0200

    Tarea semana 3

--- week-3/Homework/A-timeout/exercise.js
+++ week-3/Homework/A-timeout/exercise.js
@@ -1,11 +1,28 @@
 /*
 ================
 EXERCISE 1
-
 Task 1
 Using setTimeout, change the background colour of the page after 5 seconds (5000 milliseconds). Hint: try searching for setTimeout. (https://www.w3schools.com/jsref/met_win_settimeout.asp)
-
 Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
 ================
 */
+
+const main = document.querySelector('#main');
+
+setTimeout(() => { 
+    main.style.backgroundColor = 'pink';
+},5000)
+
+const colors = ['orange', 'purple', 'pink', 'brown', 'black', 'white'];
+let index = 0;
+
+setTimeout(() => {
+    setInterval(() => { 
+        if (index >= colors.length) {
+            index = 0;
+        }
+        main.style.backgroundColor = colors[index];
+        index++;
+    }, 5000);
+},6000);
--- week-3/Homework/C-alarmclock/alarmclock.js
+++ week-3/Homework/C-alarmclock/alarmclock.js
@@ -1,4 +1,29 @@
-function setAlarm() {}
+let intervalID;
+
+function setAlarm() {
+  let alarmSet = document.getElementById('alarmSet').value;
+  const timeRemaining = document.getElementById('timeRemaining');
+  const body = document.querySelector('body');
+
+  clearInterval(intervalID)
+  body.style.background = 'white';
+  pauseAlarm();
+
+  intervalID = setInterval(()=>{
+  
+    if(alarmSet===0){
+      timeRemaining.innerText = `Time Remaining: 00:0${alarmSet}`;
+      body.style.background = 'tomato';
+      playAlarm();
+      clearInterval(intervalID)
+    }else{
+      timeRemaining.innerText = `Time Remaining: 00:0${alarmSet}`;
+      alarmSet --
+    }
+
+  },1000)
+
+}
 
 // DO NOT EDIT BELOW HERE
 
